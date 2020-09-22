d7a8a55778b5a91bc7d7dcb4612b4d5ee2b1aa46
unknown <shiwari16@gmail.com>
2020-09-22 19:18:55 -0300

    make it better

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+import random
+import os
+
+client = commands.Bot(command_prefix = '.')
+
+##LINKU STARTOO
+
+@client.event
+async def on_ready():
+    print(f'Ok {client.user.name} is Online')
+
+#.hey é para solitarios como eu que tem seu amigo por um comando
+
+@client.command()
+async def hey(ctx):
+    await ctx.send(f'Hey how are you {ctx.author}?')
+
+##commando para ver a latencia é importante mas não vou manter e vou guardar tbm
+
+@client.command()
+async def lag(ctx):
+    await ctx.send(f'Oh look! {round(client.latency * 1000)}ms')
+
+##Comando do 8ball eu vou deixar isso de lado depois no final do dia e vou separar o codigo dele pra estudo
+
+@client.command(aliases = ['8ball', 'kamisama'])
+async def _8ball(ctx, *, question):
+    responses = ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.", 
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+    await ctx.send(f'Questions: {question}\nAnswer: {random.choice(responses)}')
+
+client.run(os.environ'8BALL_TOKEN')
