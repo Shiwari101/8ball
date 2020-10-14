c311243e051979a17bf8d4b2900c4c2a0335559c
Shiwari <64597370+Shiwari101@users.noreply.github.com>
2020-10-14 02:00:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def hey(ctx):
 
 @client.command()
 async def ping(ctx):
-    embed = discord.Embed(title = f'''Pong! {round(client.latency / 1000)}ms''', colour = discord.Colour.blue())
+    embed = discord.Embed(title = f'''Pong! {round(client.latency * 1000 / 1000)}ms''', colour = discord.Colour.blue())
     await ctx.send(embed=embed)  
 
 ##Comando do 8ball eu vou deixar isso de lado depois no final do dia e vou separar o codigo dele pra estudo
