845328c7a926f34a94665103b1ce8d52545ceda2
Shiwari <64597370+Shiwari101@users.noreply.github.com>
2021-07-05 17:35:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import discord
 from discord.ext import commands
 from modules.cactusapi import *
 
-intents = discord.Intents.default()
-intents.members = True
+
 extensions = ['kamisama', 'funfact', 'minigames', 'hey', 'ping', 'kamihelp', 'level', 'wallpaper', 'Moderation']
 cactus = CactusAPI("9RJWwx2emnj9pRrjXUzpaTByBLYvazrzjftYw3UHgmZKR")
 client = commands.Bot(command_prefix = 'k.', intents=intents)
