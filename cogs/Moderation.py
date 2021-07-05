61658018c3e43b4c0069ce99f0e833b452d8f2f0
Shiwari <64597370+Shiwari101@users.noreply.github.com>
2021-07-05 17:46:40 -0300

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,8 +1,10 @@
+import aiofiles
 import discord
 from discord.ext import commands
 from discord.ext.commands.errors import MissingPermissions
 
-
+intents = discord.Intents.default()
+intents.members = True
 client = commands.Bot(command_prefix="!", intents=intents)
 
 class Moderation(commands.Cog):
